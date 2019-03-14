afbfd0fc35981225ea06aee6a46a8addf61a6299
Phabbits <gabrieljfrey@gmail.com>
2019-03-14 17:43:58 -0400

    Gra files don't override common?
    Do not think files in GRA overider those in common, added changes to GRA to common

--- OpenRA.Mods.Common/Traits/World/ValidateOrder.cs
+++ OpenRA.Mods.Common/Traits/World/ValidateOrder.cs
@@ -1,4 +1,4 @@
-#region Copyright & License Information
+﻿#region Copyright & License Information
 /*
  * Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
  * This file is part of OpenRA, which is free software. It is made
@@ -14,35 +14,35 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.Common.Traits
 {
-	[Desc("Used to detect exploits. Attach this to the world actor.")]
-	public class ValidateOrderInfo : TraitInfo<ValidateOrder> { }
+    [Desc("Used to detect exploits. Attach this to the world actor.")]
+    public class ValidateOrderInfo : TraitInfo<ValidateOrder> { }
 
-	public class ValidateOrder : IValidateOrder
-	{
-		public bool OrderValidation(OrderManager orderManager, World world, int clientId, Order order)
-		{
-			if (order.Subject == null || order.Subject.Owner == null)
-				return true;
+    public class ValidateOrder : IValidateOrder
+    {
+        public bool OrderValidation(OrderManager orderManager, World world, int clientId, Order order)
+        {
+            if (order.Subject == null || order.Subject.Owner == null)
+                return true;
 
-			var subjectClientId = order.Subject.Owner.ClientIndex;
-			var subjectClient = orderManager.LobbyInfo.ClientWithIndex(subjectClientId);
+            var subjectClientId = order.Subject.Owner.ClientIndex;
+            var subjectClient = orderManager.LobbyInfo.ClientWithIndex(subjectClientId);
 
-			if (subjectClient == null)
-			{
-				Game.Debug("Order sent to {0}: resolved ClientIndex `{1}` doesn't exist", order.Subject.Owner.PlayerName, subjectClientId);
-				return false;
-			}
+            if (subjectClient == null)
+            {
+                Game.Debug("Order sent to {0}: resolved ClientIndex `{1}` doesn't exist", order.Subject.Owner.PlayerName, subjectClientId);
+                return false;
+            }
 
-			var isBotOrder = subjectClient.Bot != null && clientId == subjectClient.BotControllerClientIndex;
+            var isBotOrder = subjectClient.Bot != null && clientId == subjectClient.BotControllerClientIndex;
 
-			// Drop exploiting orders
-			if (subjectClientId != clientId && !isBotOrder)
-			{
-				Game.Debug("Detected exploit order from client {0}: {1}", clientId, order.OrderString);
-				return false;
-			}
+            // Drop exploiting orders
+            if (subjectClientId != clientId && !isBotOrder)
+            {
+                //Game.Debug("Detected exploit order from client {0}: {1}", clientId, order.OrderString); //MODCODE
+                //return false;
+            }
 
-			return order.Subject.AcceptsOrder(order.OrderString);
-		}
-	}
+            return order.Subject.AcceptsOrder(order.OrderString);
+        }
+    }
 }
